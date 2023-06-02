2bc310d7407077b0b05f1b2e6eafab33934f9e5c
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-06-01 20:25:03 -0500

    python login to website

---
+++ python_login.py
@@ -0,0 +1,19 @@
+# python requests login
+import requests
+
+url = 'http://127.0.0.1:5000/login_page?'
+data = {'username':'admin',
+        'password':'admin'}
+
+response = requests.post(url=url, data=data, allow_redirects=True)
+
+if response.status_code == 200:
+    print('Successful login.')
+elif response.status_code == 302:
+    print('Redirect login')
+else:
+    print('Failed login')
+
+print(response.content)
+
+
