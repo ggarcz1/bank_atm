e8bc773cc1f739c9eb6577f5427fb910bde37055
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-06-02 09:57:33 -0500

    d
    add faker items to login script, add new automation on new _user with faker, added obfuscation to password objects on login and account creation page

--- python_login.py
+++ python_login.py
@@ -1,17 +1,27 @@
-# python requests login
-import requests
+import requests, faker
 
 url = 'http://127.0.0.1:5000/login_page?'
 data = {'username':'admin',
         'password':'admin'}
 
-response = requests.post(url=url, data=data, allow_redirects=True)
+fake_user = faker.name()
+fake_password = faker.password()
+fake_email = faker.emai()
 
-if response.status_code == 200:
+data_new_user = {'username':'',
+                'password':'',
+                'email':''}
+# new user
+response_new_user = requests.post(url=url, data=data_new_user, allow_redirects=True)
+
+# login
+response_login = requests.post(url=url, data=data, allow_redirects=True)
+
+if response_login.status_code == 200:
     print('Successful login.')
-elif response.status_code == 302:
+elif response_login.status_code == 302:
     print('Redirect login')
 else:
     print('Failed login')
 
-print(response.content)
+print(response_login.content)
