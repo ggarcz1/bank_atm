00947e79b1d0162fea604e02024e97e201df5c06
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-06-02 13:44:00 -0500

    a

--- test_database.py
+++ test_database.py
@@ -5,16 +5,18 @@ cursor = sqliteConnection.cursor()
 # username = 'admin'
 # cursor.execute("SELECT password FROM users WHERE username=?", (username,))
 # print(cursor.fetchall())
+# cursor.execute("DELETE FROM users WHERE username=?",('John',))
+
 cursor.execute("SELECT * FROM users",)
 lst = cursor.fetchall()
 print('Username, Password, Email')
 for idx in range(len(lst)):
     print(lst[idx])
 
+
 # print(cursor.fetchall())
 # values = ['John','Doe','johnDoe@example.com']
 # cursor.execute("INSERT INTO users VALUES (?, ?, ?)",values)
-# cursor.execute("DELETE FROM users WHERE username=?",('a',))
 
 sqliteConnection.commit()
 sqliteConnection.close()
