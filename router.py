234d5ca540afb9666ce6be19e39fb69034ed8052
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2024-05-23 16:59:11 -0500

    Update router.py

--- router.py
+++ router.py
@@ -1,3 +1,19 @@
+# data passed in has the following format
+data = {'logon': {'nonce':'', 
+    'username':'',
+    'password':'',
+    'pin':''},
+    'update': {'','',},
+}
+data = {'logon': {'nonce':'', 
+    'username':'',
+    'password':'',
+    'pin':''},
+    'set': {''},
+}
+data = {'send': {},
+}
+
 class Router:
     def __init__(self, hostname='', ip='') -> None:
         self.hostname = hostname
@@ -22,18 +38,36 @@ class Router:
         
     # admin items
     # verify admin user 
-    def verifyAdmin(username, password, pin) -> bool:
-        return True
+    def verifyAdmin(username: str, password: str, pin: str) -> bool:
+        # static for testing
+        if username == 'admin' and password == 'admin' and pin == '1234':
+            return True
+
+        return False
         
     # set router ip
-    def setRouter(self, dest_ip, src_ip, data) -> bool:
-        verified = verifyAdmin(username, password, pin)
+    def setRouter(self, dest_ip: str, src_ip: str, data: dict) -> bool:
+        # data[logon] = [nonce, username, password, pin]
+        if len(data) != 4:
+            return False
         
+        nonce = data[0]
+        username = data[1]
+        password = data[2]
+        pin = data[3]
+
+        # check hashes
+        nonce = nonce
         # input check ips here
-        return True
+
+        verified = self.verifyAdmin(data['logon'])
+        if verified:
+            return True
+            
+        return False
     
     # update router 
-    def updateRouter(self, dest_ip, src_ip, data) -> bool:
+    def updateRouter(self, dest_ip: str, src_ip: str, data: dict) -> bool:
         verified = verifyAdmin(username, password, pin)
         
         # input check ips here
